9c2f7f234dc0227eb66f47a8cab95b9684b52815
Julius Flimmel <flimmelj@gmail.com>
2021-01-20 15:44:53 +0100

    Added create page candidate function to metatransactions contract

--- src/metatransactions/contracts/MarbleMetatransactions.sol
+++ src/metatransactions/contracts/MarbleMetatransactions.sol
@@ -11,32 +11,42 @@ contract MarbleNFT {
   function transferFrom(address from, address to, uint256 tokenId) external;
 }
 
+contract MarbleNFTCandidate {
+  function createCandidate(string calldata _uri) external payable returns(uint256 index);
+  
+  // not implemented in the candidate yet :(
+  function createCandidateWithMBCFor(string calldata _uri, address _creator) external returns(uint256 index);
+}
+
 contract MarbleNFTFactory {
   MarbleNFT public marbleNFTContract;
+  MarbleNFTCandidate public marbleNFTCandidateContract;
 }
 
 /**
  * @title MarbleMetatransactions
- * @dev Contracts allows metatransactions over marble contracts.
+ * @dev Contract allowing metatransactions for Marble Dapp.
  */
 contract MarbleMetatransactions is EIP712MetaTransaction {
 
   MarbleNFTFactory public marbleNFTFactoryContract;
-  MarbleNFT public marbleNFTContract;
 
   /**
    * @param transactionsFromChainId only transactions from this chain will be supported.
    */
   constructor(MarbleNFTFactory _marbleNFTFactoryContract, uint transactionsFromChainId) public EIP712MetaTransaction("MarbleCards test", "1", transactionsFromChainId) {
 		marbleNFTFactoryContract = _marbleNFTFactoryContract;
-    marbleNFTContract = _marbleNFTFactoryContract.marbleNFTContract();
 	}
+
+  function createPageCandidateWithMBC(string calldata uri, address creator) external {
+    marbleNFTFactoryContract.marbleNFTCandidateContract().createCandidateWithMBCFor(uri, creator);
+  }
 
   function transferNft(address toAddress, uint256 tokenId) external {
     address issuer = msgSender();
     
-    marbleNFTContract.forceApproval(tokenId, address(this));
-    marbleNFTContract.safeTransferFrom(issuer, toAddress, tokenId);
+    marbleNFTFactoryContract.marbleNFTContract().forceApproval(tokenId, address(this));
+    marbleNFTFactoryContract.marbleNFTContract().safeTransferFrom(issuer, toAddress, tokenId);
   }
 
   /** TEST FUNCTION */
@@ -45,7 +55,7 @@ contract MarbleMetatransactions is EIP712MetaTransaction {
   }
 
   function getContracts() external view returns (address, address) {
-    return (address(marbleNFTFactoryContract), address(marbleNFTContract));
+    return (address(marbleNFTFactoryContract), address(marbleNFTFactoryContract.marbleNFTContract()));
   }
 
 }
