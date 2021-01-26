e4bdc55f76ef81b93ad841543976fa90d4a7e9ba
Julius Flimmel <flimmelj@gmail.com>
2021-01-26 16:27:17 +0100

    Fix marble bank migration

--- src/core/migrations/6_deploy_marble_bank.js
+++ src/core/migrations/6_deploy_marble_bank.js
@@ -7,6 +7,6 @@ module.exports = async function(deployer) {
   const candidate = await MarbleCandidate.deployed();
   const bank = await MarbleBank.deployed();
 
-  await candidate.setBankContract(bank);
-  await bank.addAffiliate(candidate);
+  await candidate.setBankContract(bank.address);
+  await bank.addAffiliate(candidate.address);
 };
