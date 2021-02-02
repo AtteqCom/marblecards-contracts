4bacd7066fe48934bcf3e077c60f0186739b2c88
Julius Flimmel <flimmelj@gmail.com>
2021-02-02 15:49:16 +0100

    Updated migration script for new contracts

--- src/metatransactions/migrations/2_deploy_metatransactions_wrapper.js
+++
@@ -1,7 +0,0 @@
-const MarbleMetatransactions = artifacts.require("./MarbleMetatransactions.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(MarbleMetatransactions, "0xc7Bf475a8bE8c70746DC5A241B7dD3C92A1a42E1", 3);
-  
-  // TODO: addAdmin to NFT contract
-};
