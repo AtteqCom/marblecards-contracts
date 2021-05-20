ad4ecbb9b769795dca678673b5c4a075f68cc155
Julius Flimmel <flimmelj@gmail.com>
2021-05-20 20:28:29 +0200

    MarbleNFTInterface moved to utils project

--- src/bank/contracts/MarbleDutchAuction.sol
+++ src/bank/contracts/MarbleDutchAuction.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/introspection/ERC165.sol";
 import "./DutchAuctionEnumerable.sol";
-import "./MarbleNFTInterface.sol";
+import "../../utils/contracts/MarbleNFTInterface.sol";
 import "../../utils/contracts/Adminable.sol";
 import "../../utils/contracts/Pausable.sol";
 import "./MarbleDutchAuctionInterface.sol";
